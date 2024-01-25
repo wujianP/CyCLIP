c9e85203c1204f43135633d2909866f58e6c65ae
wujianP <1051233700@qq.com>
2024-01-25 12:22:45 +0800

    t

---
+++ scripts/run_download_cc3m.sh
@@ -0,0 +1,6 @@
+cd /discobox/wjpeng/code/2024/CyCLIP/utils
+conda activate /discobox/wjpeng/env/cyCLIP
+
+python download.py \
+--file='/discobox/wjpeng/dataset/cc3m/val.tsv'\
+--dir='/discobox/wjpeng/dataset/cc3m/data/val'
--- src/main.py
+++ src/main.py
@@ -29,18 +29,19 @@ from .logger import get_logger, set_logger
 mp.set_start_method("spawn", force = True)
 warnings.filterwarnings("ignore")
 
+
 def worker(rank, options, logger):
     options.rank = rank
     options.master = rank == 0
     
     set_logger(rank = rank, logger = logger, distributed = options.distributed)
 
-    if(options.device == "cuda"):
+    if options.device == "cuda":
         options.device += ":" + str(options.device_ids[options.rank] if options.distributed else options.device_id)
 
     logging.info(f"Using {options.device} device")
 
-    if(options.master):
+    if options.master:
         logging.info("Params:")
         with open(os.path.join(options.log_dir_path, "params.txt"), "w") as file:
             for key in sorted(vars(options)):
@@ -48,49 +49,49 @@ def worker(rank, options, logger):
                 logging.info(f"{key}: {value}")
                 file.write(f"{key}: {value}\n")
 
-    if(options.distributed):
+    if options.distributed:
         dist.init_process_group(backend = options.distributed_backend, init_method = options.distributed_init_method, world_size = options.num_devices, rank = options.rank)
     
     options.batch_size = options.batch_size // options.num_devices
 
     model, processor = load_model(name = options.model_name, pretrained = options.pretrained)
 
-    if(options.device == "cpu"):
+    if options.device == "cpu":
         model.float()
     else:
         torch.cuda.set_device(options.device_ids[options.rank] if options.distributed else options.device_id)
         model.to(options.device)
-        if(options.distributed):
+        if options.distributed:
             model = DDP(model, device_ids = [options.device_ids[options.rank]])
         
     data = load_data(options, processor)
 
     optimizer = None
     scheduler = None
-    if(data["train"] is not None):        
+    if data["train"] is not None:
         weight_decay_parameters = []
         no_weight_decay_parameters = []
 
         for name, parameter in model.named_parameters():
-            if(all(key not in name for key in ["bn", "ln", "bias", "logit_scale"]) and parameter.requires_grad):
+            if all(key not in name for key in ["bn", "ln", "bias", "logit_scale"]) and parameter.requires_grad:
                 weight_decay_parameters.append(parameter)
                 
-            if(any(key in name for key in ["bn", "ln", "bias", "logit_scale"]) and parameter.requires_grad):
+            if any(key in name for key in ["bn", "ln", "bias", "logit_scale"]) and parameter.requires_grad:
                 no_weight_decay_parameters.append(parameter)
 
         optimizer = optim.AdamW([{"params": no_weight_decay_parameters, "weight_decay": 0}, {"params": weight_decay_parameters, "weight_decay": options.weight_decay}], lr = options.lr, betas = (options.beta1, options.beta2), eps = options.eps)
         scheduler = cosine_scheduler(optimizer, options.lr, options.num_warmup_steps, data["train"].num_batches * options.epochs)
 
     start_epoch = 0
-    if(options.checkpoint is not None):
-        if(os.path.isfile(options.checkpoint)):
+    if options.checkpoint is not None:
+        if os.path.isfile(options.checkpoint):
             checkpoint = torch.load(options.checkpoint, map_location = options.device)
             start_epoch = checkpoint["epoch"]
             state_dict = checkpoint["state_dict"]
-            if(not options.distributed and next(iter(state_dict.items()))[0].startswith("module")):
+            if not options.distributed and next(iter(state_dict.items()))[0].startswith("module"):
                 state_dict = {key[len("module."):]: value for key, value in state_dict.items()}
             model.load_state_dict(state_dict)
-            if(optimizer is not None): optimizer.load_state_dict(checkpoint["optimizer"])
+            if optimizer is not None: optimizer.load_state_dict(checkpoint["optimizer"])
             logging.info(f"Loaded checkpoint '{options.checkpoint}' (start epoch {checkpoint['epoch']})")
         else:
             logging.info(f"No checkpoint found at {options.checkpoint}")
@@ -98,7 +99,7 @@ def worker(rank, options, logger):
     cudnn.benchmark = True
     cudnn.deterministic = False
 
-    if(options.wandb and options.master):
+    if options.wandb and options.master:
         logging.debug("Starting wandb")
         wandb.init(project = "mrl", notes = options.notes, tags = [], config = vars(options))
         wandb.run.name = options.name
@@ -106,7 +107,7 @@ def worker(rank, options, logger):
 
     evaluate(start_epoch, model, processor, data, options)
 
-    if(data["train"] is not None):
+    if data["train"] is not None:
         options.checkpoints_dir_path = os.path.join(options.log_dir_path, "checkpoints")
         os.makedirs(options.checkpoints_dir_path, exist_ok = True)
 
@@ -114,19 +115,19 @@ def worker(rank, options, logger):
 
         best_loss = np.inf
         for epoch in range(start_epoch + 1, options.epochs + 1):
-            if(options.master): 
+            if options.master:
                 logging.info(f"Starting Epoch {epoch}")
 
             start = time.time()
             train(epoch, model, data, optimizer, scheduler, scaler, options)
             end = time.time()
 
-            if(options.master): 
+            if options.master:
                 logging.info(f"Finished Epoch {epoch}, Time Taken: {end - start:.3f}")
 
             metrics = evaluate(epoch, model, processor, data, options)
 
-            if(options.master):
+            if options.master:
                 checkpoint = {"epoch": epoch, "name": options.name, "state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}
                 torch.save(checkpoint, os.path.join(options.checkpoints_dir_path, f"epoch_{epoch}.pt"))
                 if("loss" in metrics):
@@ -134,13 +135,14 @@ def worker(rank, options, logger):
                         best_loss = metrics["loss"]
                         torch.save(checkpoint, os.path.join(options.checkpoints_dir_path, f"epoch.best.pt"))
 
-    if(options.distributed):
+    if options.distributed:
         dist.destroy_process_group()
 
     if(options.wandb and options.master):
         wandb.finish()
 
-if(__name__ == "__main__"):    
+
+if __name__ == "__main__":
     options = parse_args()
 
     options.log_dir_path = os.path.join(options.logs, options.name)
