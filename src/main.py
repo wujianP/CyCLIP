9f01afa485b5e1f7e54a82f838dc3ff458e3254a
wujianP <1051233700@qq.com>
2024-01-26 17:41:41 +0800

    添加了wandb逻辑

--- src/main.py
+++ src/main.py
@@ -126,7 +126,12 @@ def worker(rank, options, logger):
 
     if options.wandb and options.master:
         logging.debug("Starting wandb")
-        wandb.init(project=options.wandb_project_name, notes=options.notes, tags=[], config=vars(options))
+        wandb.init(project=options.wandb_project_name,
+                   name=options.name,
+                   id=options.name,
+                   notes=options.notes,
+                   tags=[],
+                   config=vars(options))
         wandb.run.name = options.name
         wandb.save(os.path.join(options.log_dir_path, "params.txt"))
 
