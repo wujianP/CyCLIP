a81877d27d7c7ebab6770d05eb16a10ae2b4a545
wujianP <1051233700@qq.com>
2024-01-26 19:11:10 +0800

    添加了wandb逻辑

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,41 +1,39 @@
-# 单卡debug
+
 cd /discobox/wjpeng/code/2024/CyCLIP
 conda activate /discobox/wjpeng/env/cyCLIP
-#rm -rf /discobox/wjpeng/ckp/betterCLIP/debug
 python -m src.main \
---logs='/discobox/wjpeng/ckp/betterCLIP/debug' \
---name='exp5' \
---train_data='/discobox/wjpeng/dataset/cc3m/data/val/train_filtered.csv' \
---validation_data='/discobox/wjpeng/dataset/cc3m/data/val/train_filtered.csv' \
+--logs='/discobox/wjpeng/ckp/betterCLIP/rebuttal' \
+--name='rn50-cyclip_ep10-step200_lr1e-6-warm1000_common-cc3m-bs128_extra-wt0.2-bs8-hn2' \
+--log-per-steps=20 \
+--train_data='/discobox/wjpeng/dataset/cc3m/data/train/train_all_filtered.csv' \
 --from-pretrained='/DDN_ROOT/wjpeng/weights/cyclip/cyclip.pt' \
---image_key='image' \
---caption_key='caption' \
 --distributed \
 --device_ids 0 1 2 3 4 5 6 7 \
 --model_name='RN50' \
---batch_size=64 \
---lr=5e-6 \
---num_warmup_steps=500 \
---device='gpu' \
+--batch_size=128 \
+--lr=1e-6 \
+--epochs=10 \
+--steps-per-epoch=200 \
+--num_warmup_steps=1000 \
 --cylambda1 0.25 \
 --cylambda2 0.25 \
---log-per-steps=10 \
---epochs=10 \
---steps-per-epoch=100 \
 --extra-train-data='/DDN_ROOT/wjpeng/dataset/VL-benchmark/train/' \
 --extra-batch-size=8 \
+--extra-workers=2 \
 --extra-loss-wt=0.2 \
 --extra-data-type count existence relative-size absolute-size absolute-spatial relative-spatial \
 --extra-ann-root='/DDN_ROOT/wjpeng/dataset/VL-benchmark/train/captions_expanded/' \
---save_per_epoch=1 \
---save_most_recent \
---val-batch-size=64 \
---val-workers=8 \
+--val-batch-size=32 \
+--val-workers=6 \
 --val-common-data-root='/DDN_ROOT/wjpeng/dataset/clip_benchmark_datasets' \
 --val-vl-data-root='/DDN_ROOT/wjpeng/dataset/VL-benchmark/clean_val_v3/data' \
 --wandb \
 --wandb-project-name='test' \
 --wandb-key='8cff0498531e0409db5f3c43b52a26b0d068f2dc'
+ \
+--do-not-eval-epoch-0
+
+
 
 
 
