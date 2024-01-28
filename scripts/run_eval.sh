4174b878120dcce9f8b6c0ca8862b63f28b0e66e
wujianP <1051233700@qq.com>
2024-01-28 18:19:05 +0800

    test

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -11,8 +11,8 @@ python -m src.main \
 --val-common-data-root='/DDN_ROOT/wjpeng/dataset/clip_benchmark_datasets' \
 --val-vl-data-root='/DDN_ROOT/wjpeng/dataset/VL-benchmark/clean_val_v3/data' \
 --logs='/discobox/wjpeng/ckp/betterCLIP/rebuttal/eval' \
---name='rn50-cyclip_ep10-step400_lr1e-6-warm1000_common-cc3m-bs64_extra-wt0-bs8-hn2' \
---from-pretrained='/discobox/wjpeng/ckp/betterCLIP/rebuttal/rn50-cyclip_ep10-step400_lr1e-6-warm1000_common-cc3m-bs64_extra-wt0-bs8-hn2/checkpoints/epoch_10.pt' \
+--name='rn50-cyclip_ep10-step400_lr2e-6-warm1000_common-cc3m-bs64_extra-wt0.2-bs4-hn2-v2' \
+--from-pretrained='/discobox/wjpeng/ckp/betterCLIP/rebuttal/rn50-cyclip_ep10-step400_lr2e-6-warm1000_common-cc3m-bs64_extra-wt0.2-bs4-hn2-v2/checkpoints/epoch_10.pt' \
 --wandb \
 --wandb-project-name='cyCLIP_EVAL' \
 --wandb-key='8cff0498531e0409db5f3c43b52a26b0d068f2dc'
